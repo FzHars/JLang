f7ecd53cc6e8c3b56275abf6475e810cd70a96fc
FzHars <fawaz.io.sh@gmail.com>
2026-05-06 14:05:08 +0700

    add data

---
+++ src/data/nekoData.js
@@ -0,0 +1,135 @@
+export const nekoData = [
+  // Hiragana - Seion (清音)
+  { id: 1, char: "あ", romanization: "a", type: "hiragana", group: "seion" },
+  { id: 2, char: "い", romanization: "i", type: "hiragana", group: "seion" },
+  { id: 3, char: "う", romanization: "u", type: "hiragana", group: "seion" },
+  { id: 4, char: "え", romanization: "e", type: "hiragana", group: "seion" },
+  { id: 5, char: "お", romanization: "o", type: "hiragana", group: "seion" },
+
+  // Hiragana - Ka
+  { id: 6, char: "か", romanization: "ka", type: "hiragana", group: "ka" },
+  { id: 7, char: "き", romanization: "ki", type: "hiragana", group: "ka" },
+  { id: 8, char: "く", romanization: "ku", type: "hiragana", group: "ka" },
+  { id: 9, char: "け", romanization: "ke", type: "hiragana", group: "ka" },
+  { id: 10, char: "こ", romanization: "ko", type: "hiragana", group: "ka" },
+
+  // Hiragana - Sa
+  { id: 11, char: "さ", romanization: "sa", type: "hiragana", group: "sa" },
+  { id: 12, char: "し", romanization: "shi", type: "hiragana", group: "sa" },
+  { id: 13, char: "す", romanization: "su", type: "hiragana", group: "sa" },
+  { id: 14, char: "せ", romanization: "se", type: "hiragana", group: "sa" },
+  { id: 15, char: "そ", romanization: "so", type: "hiragana", group: "sa" },
+
+  // Hiragana - Ta
+  { id: 16, char: "た", romanization: "ta", type: "hiragana", group: "ta" },
+  { id: 17, char: "ち", romanization: "chi", type: "hiragana", group: "ta" },
+  { id: 18, char: "つ", romanization: "tsu", type: "hiragana", group: "ta" },
+  { id: 19, char: "て", romanization: "te", type: "hiragana", group: "ta" },
+  { id: 20, char: "と", romanization: "to", type: "hiragana", group: "ta" },
+
+  // Hiragana - Na
+  { id: 21, char: "な", romanization: "na", type: "hiragana", group: "na" },
+  { id: 22, char: "に", romanization: "ni", type: "hiragana", group: "na" },
+  { id: 23, char: "ぬ", romanization: "nu", type: "hiragana", group: "na" },
+  { id: 24, char: "ね", romanization: "ne", type: "hiragana", group: "na" },
+  { id: 25, char: "の", romanization: "no", type: "hiragana", group: "na" },
+
+  // Hiragana - Ha
+  { id: 26, char: "は", romanization: "ha", type: "hiragana", group: "ha" },
+  { id: 27, char: "ひ", romanization: "hi", type: "hiragana", group: "ha" },
+  { id: 28, char: "ふ", romanization: "fu", type: "hiragana", group: "ha" },
+  { id: 29, char: "へ", romanization: "he", type: "hiragana", group: "ha" },
+  { id: 30, char: "ほ", romanization: "ho", type: "hiragana", group: "ha" },
+
+  // Hiragana - Ma
+  { id: 31, char: "ま", romanization: "ma", type: "hiragana", group: "ma" },
+  { id: 32, char: "み", romanization: "mi", type: "hiragana", group: "ma" },
+  { id: 33, char: "む", romanization: "mu", type: "hiragana", group: "ma" },
+  { id: 34, char: "め", romanization: "me", type: "hiragana", group: "ma" },
+  { id: 35, char: "も", romanization: "mo", type: "hiragana", group: "ma" },
+
+  // Hiragana - Ya
+  { id: 36, char: "や", romanization: "ya", type: "hiragana", group: "ya" },
+  { id: 37, char: "ゆ", romanization: "yu", type: "hiragana", group: "ya" },
+  { id: 38, char: "よ", romanization: "yo", type: "hiragana", group: "ya" },
+
+  // Hiragana - Ra
+  { id: 39, char: "ら", romanization: "ra", type: "hiragana", group: "ra" },
+  { id: 40, char: "り", romanization: "ri", type: "hiragana", group: "ra" },
+  { id: 41, char: "る", romanization: "ru", type: "hiragana", group: "ra" },
+  { id: 42, char: "れ", romanization: "re", type: "hiragana", group: "ra" },
+  { id: 43, char: "ろ", romanization: "ro", type: "hiragana", group: "ra" },
+
+  // Hiragana - Wa
+  { id: 44, char: "わ", romanization: "wa", type: "hiragana", group: "wa" },
+  { id: 45, char: "を", romanization: "wo", type: "hiragana", group: "wa" },
+  { id: 46, char: "ん", romanization: "n", type: "hiragana", group: "wa" },
+
+  // Katakana - Seion (清音)
+  { id: 47, char: "ア", romanization: "a", type: "katakana", group: "seion" },
+  { id: 48, char: "イ", romanization: "i", type: "katakana", group: "seion" },
+  { id: 49, char: "ウ", romanization: "u", type: "katakana", group: "seion" },
+  { id: 50, char: "エ", romanization: "e", type: "katakana", group: "seion" },
+  { id: 51, char: "オ", romanization: "o", type: "katakana", group: "seion" },
+
+  // Katakana - Ka
+  { id: 52, char: "カ", romanization: "ka", type: "katakana", group: "ka" },
+  { id: 53, char: "キ", romanization: "ki", type: "katakana", group: "ka" },
+  { id: 54, char: "ク", romanization: "ku", type: "katakana", group: "ka" },
+  { id: 55, char: "ケ", romanization: "ke", type: "katakana", group: "ka" },
+  { id: 56, char: "コ", romanization: "ko", type: "katakana", group: "ka" },
+
+  // Katakana - Sa
+  { id: 57, char: "サ", romanization: "sa", type: "katakana", group: "sa" },
+  { id: 58, char: "シ", romanization: "shi", type: "katakana", group: "sa" },
+  { id: 59, char: "ス", romanization: "su", type: "katakana", group: "sa" },
+  { id: 60, char: "セ", romanization: "se", type: "katakana", group: "sa" },
+  { id: 61, char: "ソ", romanization: "so", type: "katakana", group: "sa" },
+
+  // Katakana - Ta
+  { id: 62, char: "タ", romanization: "ta", type: "katakana", group: "ta" },
+  { id: 63, char: "チ", romanization: "chi", type: "katakana", group: "ta" },
+  { id: 64, char: "ツ", romanization: "tsu", type: "katakana", group: "ta" },
+  { id: 65, char: "テ", romanization: "te", type: "katakana", group: "ta" },
+  { id: 66, char: "ト", romanization: "to", type: "katakana", group: "ta" },
+
+  // Katakana - Na
+  { id: 67, char: "ナ", romanization: "na", type: "katakana", group: "na" },
+  { id: 68, char: "ニ", romanization: "ni", type: "katakana", group: "na" },
+  { id: 69, char: "ヌ", romanization: "nu", type: "katakana", group: "na" },
+  { id: 70, char: "ネ", romanization: "ne", type: "katakana", group: "na" },
+  { id: 71, char: "ノ", romanization: "no", type: "katakana", group: "na" },
+
+  // Katakana - Ha
+  { id: 72, char: "ハ", romanization: "ha", type: "katakana", group: "ha" },
+  { id: 73, char: "ヒ", romanization: "hi", type: "katakana", group: "ha" },
+  { id: 74, char: "フ", romanization: "fu", type: "katakana", group: "ha" },
+  { id: 75, char: "ヘ", romanization: "he", type: "katakana", group: "ha" },
+  { id: 76, char: "ホ", romanization: "ho", type: "katakana", group: "ha" },
+
+  // Katakana - Ma
+  { id: 77, char: "マ", romanization: "ma", type: "katakana", group: "ma" },
+  { id: 78, char: "ミ", romanization: "mi", type: "katakana", group: "ma" },
+  { id: 79, char: "ム", romanization: "mu", type: "katakana", group: "ma" },
+  { id: 80, char: "メ", romanization: "me", type: "katakana", group: "ma" },
+  { id: 81, char: "モ", romanization: "mo", type: "katakana", group: "ma" },
+
+  // Katakana - Ya
+  { id: 82, char: "ヤ", romanization: "ya", type: "katakana", group: "ya" },
+  { id: 83, char: "ユ", romanization: "yu", type: "katakana", group: "ya" },
+  { id: 84, char: "ヨ", romanization: "yo", type: "katakana", group: "ya" },
+
+  // Katakana - Ra
+  { id: 85, char: "ラ", romanization: "ra", type: "katakana", group: "ra" },
+  { id: 86, char: "リ", romanization: "ri", type: "katakana", group: "ra" },
+  { id: 87, char: "ル", romanization: "ru", type: "katakana", group: "ra" },
+  { id: 88, char: "レ", romanization: "re", type: "katakana", group: "ra" },
+  { id: 89, char: "ロ", romanization: "ro", type: "katakana", group: "ra" },
+
+  // Katakana - Wa
+  { id: 90, char: "ワ", romanization: "wa", type: "katakana", group: "wa" },
+  { id: 91, char: "ヲ", romanization: "wo", type: "katakana", group: "wa" },
+  { id: 92, char: "ン", romanization: "n", type: "katakana", group: "wa" },
+];
+
+export default nekoData;
