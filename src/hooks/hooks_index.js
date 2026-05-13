5779f7b27d289edb821e1796ee5306eabfb7145f
FzHars <fawaz.io.sh@gmail.com>
2026-05-13 14:50:41 +0700

    -

--- src/hooks/hooks_index.js
+++
@@ -1,6 +0,0 @@
-// Hooks - Central Export
-// Re-export all custom hooks and utilities for clean imports
-
-export { useTrainingState } from "./useTrainingState.js";
-export { trainingReducer, initialState } from "./trainingReducer.js";
-export { ACTIONS } from "./trainingActions.js";
